94bbc6f293fa33dea2476df19b515c4e1b3d34e0
d3vsupp0r7 <dev@dev.com>
2019-07-09 21:02:04 +0200

    First commit

---
+++ springboot1_mvc_helloworld/src/main/java/org/lba/springboot1/controller/impl/EmployeeRestControllerImpl.java
@@ -0,0 +1,10 @@
+package org.lba.springboot1.controller.impl;
+
+import org.lba.springboot1.controller.EmployeeRestController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRestControllerImpl implements EmployeeRestController {
+
+	
+}
